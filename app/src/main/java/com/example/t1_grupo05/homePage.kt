ed4aea81a8c51c91509b921205c955dda5fff817
malvba <meli.gi@hotmail.com>
2024-05-02 14:01:44 -0500

    interfaces

--- app/src/main/java/com/example/t1_grupo05/homePage.kt
+++ app/src/main/java/com/example/t1_grupo05/homePage.kt
@@ -1,20 +1,48 @@
 package com.example.t1_grupo05
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 
 class homePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        }
+
+    }
+
+    fun SiguienteCuenta (view: View){
+        val siguientecuenta = Intent(this, accountPage::class.java )
+        startActivity(siguientecuenta)
+    }
+
+
+    fun ocultarSaldo(view: View) {
+        val textView = findViewById<TextView>(R.id.textView12)
+        if (textView != null && textView.isVisible) {
+            textView.visibility = View.INVISIBLE
         }
     }
+
+
+
+
 }
