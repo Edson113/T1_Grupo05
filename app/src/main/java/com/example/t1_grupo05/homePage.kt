efc8a3c221d0ecc8bf917f21fac55e28960d7226
malvba <meli.gi@hotmail.com>
2024-05-02 14:45:05 -0500

    ahora si

--- app/src/main/java/com/example/t1_grupo05/homePage.kt
+++ app/src/main/java/com/example/t1_grupo05/homePage.kt
@@ -1,10 +1,14 @@
 package com.example.t1_grupo05
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class homePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,24 @@ class homePage : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun SiguienteCuenta (view: View){
+        val siguientecuenta = Intent(this, accountPage::class.java )
+        startActivity(siguientecuenta)
+    }
+
+    fun ocultarSaldo (view: View){
+        val cuentaahorro = findViewById<TextView>(R.id.textView12)
+        val tarjetainterbank = findViewById<TextView>(R.id.textView15)
+
+        if (cuentaahorro.visibility == View.VISIBLE && tarjetainterbank.visibility == View.VISIBLE) {
+            cuentaahorro.visibility = View.GONE
+            tarjetainterbank.visibility = View.GONE
+        } else {
+            cuentaahorro.visibility = View.VISIBLE
+            tarjetainterbank.visibility = View.VISIBLE
         }
     }
 }
